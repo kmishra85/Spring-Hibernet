0eb722f1db7d5737ac39e0588c0513b46f88abe1
kmishra85 <mishrakeshav50@gmail.com>
2017-10-19 12:39:58 +0530

    Add files via upload

---
+++ spring/DemoSpring_7/src/com/igate/Employee.java
@@ -0,0 +1,15 @@
+package com.igate;
+
+import java.sql.Date;
+
+public class Employee {
+
+	Date date;
+	
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+}
